cdbf7b7d79c9ab3ac50da38685c02e4d22cdb1a7
Lamool <gosu0522@naver.com>
2025-08-31 19:40:39 +0900

    250831_상품 이미지, 가격 배열 문제 해결
    기존에는 상품 이미지와 가격을 각각의 배열로 관리해서 어떤 상품에 대한 가격인지 알 수 없었고, 이미지 순서가 바뀌면 엉켜버리는 문제가 있었음.
    상품 이미지와 가격을 클래스로 묶어서 관리하도록 변경함으로써 문제를 해결함.

--- Assets/02.Scripts/ItemManager.cs
+++ Assets/02.Scripts/ItemManager.cs
@@ -4,12 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]   // 꼭 있어야 인스펙터에 보인다
+public class SaleItem
+{
+    public Sprite itemImg;      // 편의점 상품 이미지
+    public int itemPrice;       // 편의점 상품 가격
+}
+
 public class ItemManager : MonoBehaviour
 {
     public GameObject[] itemArr;
     bool isShow;
-    public Sprite[] saleItem;       // 상품을 담은 배열
-    int[] itemPrice = { 1600, 2500, 2700, 2000, 1000, 300, 1000, 1500, 1700, 1500, 1200, 4500, 1600 };      // 상품 가격을 담은 배열
+    public SaleItem[] saleItems;
     int saleSum = 0;
     int count = 0;
     List<int> list;
@@ -24,8 +30,8 @@ public class ItemManager : MonoBehaviour
         {
             isShow = Random.Range(0, 10) > 6;
 
-            idx = Random.Range(0, saleItem.Length);
-            itemArr[i].GetComponent<Image>().sprite = saleItem[idx];
+            idx = Random.Range(0, saleItems.Length);
+            itemArr[i].GetComponent<Image>().sprite = saleItems[idx].itemImg;
             itemArr[i].gameObject.SetActive(isShow);
             if (isShow)
             {
@@ -35,8 +41,8 @@ public class ItemManager : MonoBehaviour
         }
         if (count == 0)
         {
-            idx = Random.Range(0, saleItem.Length);
-            itemArr[0].GetComponent<Image>().sprite = saleItem[idx];
+            idx = Random.Range(0, saleItems.Length);
+            itemArr[0].GetComponent<Image>().sprite = saleItems[idx].itemImg;
             itemArr[0].gameObject.SetActive(true);
             count++;
             list.Add(idx);
@@ -47,7 +53,7 @@ public class ItemManager : MonoBehaviour
     {
         for (int i = 0; i < list.Count; i++)
         {
-            saleSum += itemPrice[list[i]];
+            saleSum += saleItems[list[i]].itemPrice;
         }
         gameObject.GetComponent<UIManager>().priceText.text = saleSum.ToString() + "원";
     }
